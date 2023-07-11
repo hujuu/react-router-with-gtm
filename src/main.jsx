21b71d0c01c001dda8d8182a27ec754a67193f84
hujuu <1961946+hujuu@users.noreply.github.com>
2023-07-11 22:46:10 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,22 +7,32 @@ import {
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
+import { HelmetProvider } from 'react-helmet-async';
+import TagManager from 'react-gtm-module';
 import './index.css'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-NV8BP88'    // replace this with your GTM ID
+}
+
+TagManager.initialize(tagManagerArgs)
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root />,
+        element: <Root title="Home" />,
         errorElement: <ErrorPage />,
     },
     {
         path: "contacts/:contactId",
-        element: <Contact />,
+        element: <Contact title="Contact" />,
     },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 )
